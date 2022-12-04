6b3918c73f8715f07d3b047d0518d0e657b14fc0
jkC-1995 <jakbustin95@gmail.com>
2022-12-04 08:43:35 +0000

    day 2 p2 complete

--- Day2/day2.py
+++ Day2/day2.py
@@ -8,8 +8,6 @@ with open('input.txt', 'r') as f:
     lines = f.readlines()
     hands = [entry.strip() for entry in lines]
 
-print(hands)
-
 
 map_input = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors',
              'X': 'Rock', 'Y': 'Paper', 'Z': 'Scissors'}
@@ -32,4 +30,48 @@ def points_per_round(choice):
     return 0
 
 
+print("Part 1: ")
 print(sum([points_per_round(choice) for choice in hands]))
+
+map_input2 = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors',
+              'X': 'Lose', 'Y': 'Draw', 'Z': 'Win'}
+points_per_shape2 = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
+points_per_outcome2 = {'Lose': 0, 'Draw': 3, 'Win': 6}
+
+
+def part2(choice):
+    opponent_shape = map_input2[choice[0]]
+    game_result = map_input2[choice[2]]
+
+    # if game result is draw:
+
+    if game_result == 'Draw':
+        return points_per_outcome2[game_result] + points_per_shape2[opponent_shape]
+
+    elif game_result == 'Lose':
+        if opponent_shape == 'Paper':
+            return points_per_outcome2[game_result] + points_per_shape2['Rock']
+        if opponent_shape == 'Rock':
+            return points_per_outcome2[game_result] + points_per_shape2['Scissors']
+        if opponent_shape == 'Scissors':
+            return points_per_outcome2[game_result] + points_per_shape2['Paper']
+
+    elif game_result == 'Win':
+        if opponent_shape == 'Scissors':
+            return points_per_outcome2[game_result] + points_per_shape2['Rock']
+        if opponent_shape == 'Paper':
+            return points_per_outcome2[game_result] + points_per_shape2['Scissors']
+        if opponent_shape == 'Rock':
+            return points_per_outcome2[game_result] + points_per_shape2['Paper']
+
+
+# Day2
+game_ending = {'X': 'Lose', 'Y': 'Draw', 'Z': 'Win'}
+
+print("Part2: ")
+print(sum([part2(choice) for choice in hands]))
+
+# determine if need to win, lose or draw
+
+
+# x lose, y draw, z win
